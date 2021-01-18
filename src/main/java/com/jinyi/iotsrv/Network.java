ae9ff5c325b5b869d1df615a923edf825c549296
Error-LinuxKernelCrashed <shy2354538846@outlook.com>
2021-01-18 13:13:17 +0800

    JSON解析

--- src/main/java/com/jinyi/iotsrv/Network.java
+++ src/main/java/com/jinyi/iotsrv/Network.java
@@ -1,13 +1,19 @@
 package com.jinyi.iotsrv;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import com.alibaba.fastjson.*;
 import java.net.ServerSocket;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 import java.net.Socket;
+import java.util.Set;
 public class Network {
     ServerSocket srvSocket;
     public Network(int port){
         try{
             srvSocket = new ServerSocket(port);
             System.out.println("开始监听" + port + "端口");
+            // 创建ServerSocket,开始监听端口
         } catch (IOException e){
             e.printStackTrace();
         }
@@ -23,7 +29,7 @@ public class Network {
                 } finally{
                     if (clientSocket != null){
                         try{
-                            
+                            MsgHandler(clientSocket);
                             // Handling Client Connect Event
                             clientSocket.close();
                             clientSocket = null;
@@ -32,6 +38,33 @@ public class Network {
                         }
                     }
                 }
+            }
+        }
+        private void MsgHandler(Socket socket){
+            int length;
+            String rcevString = "";
+            byte[] rcevBuffer = new byte[1024];
+            try {
+                InputStream clientInputStream = socket.getInputStream();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(64000);
+                while((length = clientInputStream.read(rcevBuffer)) != -1){
+                    byteArrayOutputStream.write(rcevBuffer, 0, length);
+                    rcevString = byteArrayOutputStream.toString("UTF-8");
+                    byteArrayOutputStream = new ByteArrayOutputStream();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(rcevString != ""){
+                JSONObject msgJsonObject = JSONObject.parseObject(rcevString);
+                // Convert String into JSON Object
+                String ackTypeString = msgJsonObject.getString("ackType");
+                switch (ackTypeString){
+                    // TODO:在此添加消息响应
+                    default: System.out.println("非法请求");
+                }
+            } else {
+                System.out.println("非法请求");
             }
         }
     }
